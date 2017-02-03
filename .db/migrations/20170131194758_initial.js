60c1e30e485e95b89df6d1c91e4744982cb598d9
Andrew Crist <andrew.l.crist@gmail.com>
2017-02-03 07:51:55 -0700

    Update Migrations for heroku

--- .db/migrations/20170131194758_initial.js
+++ .db/migrations/20170131194758_initial.js
@@ -15,6 +15,6 @@ exports.up = function(knex, Promise) {
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-    knex.schema.dropTable('polls')
+    knex.schema.dropTableIfExists('polls')
   ])
 };
